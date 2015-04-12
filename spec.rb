a154a8013b31ada47886fe125993440b7574cf4f
Josh Cheek <josh.cheek@gmail.com>
2015-04-12 02:06:19 -0600

    Identifiers can have underscores

--- spec.rb
+++ spec.rb
@@ -11,13 +11,13 @@ module SpecHelpers
     compile:      true
 
   def parses!(body, assertions)
-    ast! parse(body), assertions
+    ast! parse(body, errstream: $stderr), assertions
   end
 
-  def parse(body)
+  def parse(body, errstream: StringIO.new)
     Parse body:         body,
           parser_class: SpecHelpers.parser_class,
-          errstream:    StringIO.new,
+          errstream:    errstream,
           pry:          false
   end
 
@@ -43,37 +43,65 @@ RSpec.describe 'My language' do
     alias are it
   end
 
-  example 'sanity test: some basic parsing' do
-    parses! 'x', terminals_include: 'x'
+  context 'parsing' do
+    example 'sanity test: some basic parsing' do
+      parses! 'x', terminals_include: 'x'
+    end
+
+    example 'identifiers are lowercase a-z and underscores' do
+      parses! 'x',   expr: { type: :identifier, value: 'x' }
+      parses! '_',   expr: { type: :identifier, value: '_' }
+      parses! '_x_', expr: { type: :identifier, value: '_x_' }
+      parses! 'a_b', expr: { type: :identifier, value: 'a_b' }
+      parses! [*'a'..'z'].join, expr: {
+        type: :identifier,
+        value: 'abcdefghijklmnopqrstuvwxyz',
+      }
+    end
+
+    example 'local vars are bare identifiers'
+    example 'ivars are identifiers with an @ prefix'
+    example 'method calls are to the RHS of a "."'
+    example 'lines beginning with method calls are invoked on the result of the previous line'
+    example 'setters are method calls on the LHS of an assignment arrow'
   end
 
-  context 'entry locations' do
-    it 'exports named classes'
-    it 'exports anonymous functions to call'
-  end
+  context 'running', pending: true do
+    context 'entry locations' do
+      it 'can be anonymous functions' do
+        e = eval '-> (x) x + x'
+        expect(e.call 11).to eq 22
+      end
 
-  context 'self' do
-    it 'is represented with @'
-    it 'calls methods on self with @.method'
-  end
+      it 'can be named classes' do
+        e = eval '-> :A ()'
+        expect(e::A).to be_a_kind_of Class
+      end
+    end
 
-  context 'the assigment arrow' do
-    it 'assigns locals'
-    it 'assigns ivars'
-    it 'calls setters'
-  end
+    context 'self' do
+      it 'is represented with @'
+      it 'calls methods on self with @.method'
+    end
 
-  context 'blocks' do
-    are 'indicated by an arg list and inline body'
-    are 'indicated by an arg list and indentation'
-    are 'indicated by indentation without an arg list'
-  end
+    context 'the assigment arrow' do
+      it 'assigns locals'
+      it 'assigns ivars'
+      it 'calls setters'
+    end
 
-  context 'local variables' do
-  end
+    context 'blocks' do
+      are 'indicated by an arg list and inline body'
+      are 'indicated by an arg list and indentation'
+      are 'indicated by indentation without an arg list'
+    end
 
-  context 'argument lists' do
-    are 'indicated by parentheses'
-    #...
+    context 'local variables' do
+    end
+
+    context 'argument lists' do
+      are 'indicated by parentheses'
+      #...
+    end
   end
 end
